8d7e1e86683191acc3dd143aac26c1c7e00d4dbb
difersanchez <difersanchez@gmail.com>
2026-04-09 16:55:17 -0300

    Fix FastQC memory errors: add memory managing for JVM and implemented only serial FastQC in the end

--- scripts/qc/01_fastqc_raw.sh
+++ scripts/qc/01_fastqc_raw.sh
@@ -4,4 +4,8 @@ set -euo pipefail
 source config/project.env
 mkdir -p results/qc/raw
 
-fastqc -t "$THREADS" -o results/qc/raw "$R1" "$R2"
+# Removing parallelization because it messed up the HPC mem allocation
+#fastqc -t 2 -o results/qc/raw "$R1" "$R2"
+
+fastqc -o results/qc/raw "$R1" "$R2"
+
--- scripts/qc/03_fastqc_trimmed.sh
+++ scripts/qc/03_fastqc_trimmed.sh
@@ -4,6 +4,8 @@ set -euo pipefail
 source config/project.env
 mkdir -p results/qc/trimmed
 
-fastqc -t "$THREADS" -o results/qc/trimmed \
+#Parallelization removed for messing with JVM allocation.
+#fastqc -t 2 -o results/qc/trimmed \
+fastqc -o results/qc/trimmed \
   "results/trim/${SAMPLE}_R1.trim.fq.gz" \
   "results/trim/${SAMPLE}_R2.trim.fq.gz"
